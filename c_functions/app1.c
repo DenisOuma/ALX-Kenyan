0d119e6f30bde1234f43243d007851365ed5ba43
DenisOuma <dennisouma38@gmail.com>
2021-05-31 16:17:06 -0400

    Branch develop created

---
+++ c_functions/app1.c
@@ -0,0 +1,4 @@
+/* 
+1. Write a C function that can perform a multiplication of two integers and return the
+calculated result.
+*/
---
+++ c_functions/app2.c
@@ -0,0 +1,5 @@
+/* 
+2. Write a C program that calls the C function you just wrote in app1.c to calcu-
+late the multiplication of 3 times 5 and then print out the return value from the
+function on the screen.
+*/
